54e2f39aa843a268c7e51536a542c5c9c17a35e6
Arthur Gueler <hyoshi.pv@gmail.com>
2026-03-02 23:57:43 -0300

    fix: SSL config para Aiven MySQL

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: process.env.NODE_ENV === 'production' ? 'require' : undefined,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
